6db7c429844ef993cf029b9ae9e3cd931df5cee1
Chad Buehrle <chad.buehrle@gmail.com>
2016-12-22 14:37:55 -0600

    initial

---
+++ client/js/script.js
@@ -0,0 +1,11 @@
+(() => {
+  var mountNode = document.querySelector('#some-selector');
+
+class HelloMessage extends React.Component {
+  render() {
+    return <div>Hello Bob!</div>;
+  }
+}
+
+ReactDOM.render(<HelloMessage />, mountNode);
+})()
